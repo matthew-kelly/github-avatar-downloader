b5f40eafdee36e9aee34621ae0f79a5a60e785fa
Matt Kelly <bmatthewkelly@gmail.com>
2018-07-09 23:57:22 +0000

    modified callback function to pass JSON object

--- download_avatars.js
+++ download_avatars.js
@@ -13,14 +13,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-      cb(err, body);
+    body = JSON.parse(body);
+    body.forEach(function(user){
+      console.log(user.avatar_url);
     })
-    .on('error', function(err) {
-      throw err;
-    })
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", JSON.parse(result));
+  // console.log("Result:", result);
 });
