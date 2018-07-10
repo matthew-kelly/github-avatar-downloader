8d98ce1c71aa11139d9c9df2bce33cc5b3ae28cd
Matt Kelly <bmatthewkelly@gmail.com>
2018-07-10 03:42:00 +0000

    added first three error clauses from stretch work

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,4 @@
+// coded with @comberj and @rafrocha
 // requires
 require('dotenv').config();
 var request = require('request');
@@ -8,11 +9,10 @@ var args = process.argv;
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  if (args.length !== 4) { // incorrect number of args
-    console.log('Incorrect number of arguments!')
-    throw Error();
+  if (!repoOwner || !repoName) { // incorrect number of args
+    console.log('Error: Incorrect number of arguments!');
+    return;
   }
-
   var options = { // url parameters
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
@@ -21,34 +21,41 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
   request(options, function(err, res, body) {
+    if (res.statusCode === 404) {
+      console.log('Error: Owner/Repo combo does not exist');
+      return;
+    }
     cb(err, body); // data passed to callback function
   });
 }
 
 function downloadImageByURL(url, filePath) {
-  if (!fs.existsSync('./avatars/')) {
-    mkdirp('./avatars/');
-  }
   request.get(url)
     .on('error', function(err) { // error for failed request
-      throw err;
+      console.log('Error: Network Issue');
+      return;
     })
     .on('response', function(response) { // error code for successful request (not a 2xx status code)
       if (response.statusCode < 200 || response.statusCode >= 300) {
         console.log('Error Code: ', response.statusCode);
-        throw Error();
+        return;
       }
-      console.log('Download started');
+      console.log('Download started: ', filePath);
     })
     .on('end', function() {
-      console.log('Download complete');
+      console.log('Download complete: ', filePath);
     })
-    .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'));
+    .pipe(fs.createWriteStream(filePath));
 }
 
 getRepoContributors(args[2], args[3], function(err, result) {
   var userData = JSON.parse(result);
+  if (!fs.existsSync('./avatars/')) {
+    mkdirp('./avatars/');
+  }
   userData.forEach(function(user) {
-    downloadImageByURL(user.avatar_url, user.login); // pass avatar url and username to downloadImageByURL function
+    var imgURL = user.avatar_url;
+    var download = './avatars/' + user.login + '.jpg';
+    downloadImageByURL(imgURL, download); // pass avatar url and desired file path to downloadImageByURL function
   });
 });
